04ed1d9358d433a1c528d1356598e5ce777b796e
Neil <neil@Neils-MacBook-Pro.local>
2015-09-28 09:46:28 -0700

    Commit invalid change

--- features/support/env.rb
+++ features/support/env.rb
@@ -15,7 +15,7 @@ end
 Before do | scenario |
 	# need to configure env variables for browser
   capabilities_config = {
-    :version => "#{ENV['version']}",
+    :version => "this is repo1 change",
     :browserName => "#{ENV['browserName']}",
     :platform => "#{ENV['platform']}",
     :name => "#{scenario.feature.name} - #{scenario.name}"
