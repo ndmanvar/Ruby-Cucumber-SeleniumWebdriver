5c4ebf92e647eaa0a1e0c8ecb798285f9c892f1c
Neil <neil@conf-3.ocbanquet.com>
2015-12-03 11:14:28 -0800

    Adding a failure

--- features/page_objects/home_page.rb
+++ features/page_objects/home_page.rb
@@ -2,7 +2,7 @@ class HomePage
   include PageObject
 
   def freeTrialButton
-  	@browser.find_element(:css => '.button.red[href*=trial]')
+  	@browser.find_element(:css => '.button.red[href*=trial-will-fail]')
   end
 
 end
